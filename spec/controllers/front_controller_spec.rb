4986326366dcf118eb28d4fdb55d5757aaf24271
alirio <alirio@Dev-Server.lan>
2018-09-17 21:08:29 -0400

    change text rspec 'it'

--- spec/controllers/front_controller_spec.rb
+++ spec/controllers/front_controller_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 
 describe FrontController, type: :routing do
   describe 'GET index' do
-    it 'renders the index view' do
+    it 'route index view' do
       get :index
       expect(get('/')).to route_to(controller: 'front', action: 'index')
     end
   end
 
   describe 'GET resume' do
-    it 'renders the index view' do
+    it 'route resume view' do
       get :resume
       expect(get('/resume')).to route_to('front#resume')
     end
