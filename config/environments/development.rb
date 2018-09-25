7f4e41839ee8399ea5bbc210bddb10d956290a6b
alirio <alirio@Dev-Server.lan>
2018-09-25 12:58:14 -0400

    resolve integration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,23 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USER"],
+    password: ENV["GMAIL_PSWD"]
+  }
+
+  config.action_mailer.default_url_options = {host: "http://192.168.1.218:3000"}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
