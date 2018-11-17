4482b3257a8d9a5b954864dbfa690a008996cf53
alirio <alirio@Dev-Server.lan>
2018-11-16 21:53:57 -0400

    validations in model Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
+  attribute :name, validate: true
+  attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :subject, validate: true
+  attribute :content, validate: true
 end
