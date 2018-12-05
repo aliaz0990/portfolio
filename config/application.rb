9222753fb76cff0336ff28601be0db0a7ee96c28
alirio <alirio@Dev-Server.lan>
2018-12-05 11:49:04 -0400

    i18n setup

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,10 @@ module Portfolio0
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.enforce_available_locales = false
     config.time_zone = 'America/Caracas'
+    config.i18n.available_locales = [:en, :es]
     config.i18n.default_locale = 'en'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+  scope'/(:locale)', defaults: { locale: 'en'}, constraints: { locale: /en|es/ } do
     root 'front#index'
     get '/resume', to: 'front#resume'
     get '/portfolio', to: 'front#portfolio'
     get '/contact', to: 'front#contact'
     post '/send_email', to: 'front#send_email', as: :send_email
   end
-  get '/:locale' => 'front#index'
-  match '*path', to: redirect("/#{I18n.default_locale}/#{path}")
-  match '', to: redirect("/#{I18n.default_locale}")
 end
