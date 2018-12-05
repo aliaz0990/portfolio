c208ae589411d97ebca929bd128045d5386fe0c8
alirio <alirio@Dev-Server.lan>
2018-12-05 09:03:58 -0400

    translate config

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root 'front#index'
-  get '/resume', to: 'front#resume'
-  get '/portfolio', to: 'front#portfolio'
-  get '/contact', to: 'front#contact'
-  post '/send_email', to: 'front#send_email', as: :send_email
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+    root 'front#index'
+    get '/resume', to: 'front#resume'
+    get '/portfolio', to: 'front#portfolio'
+    get '/contact', to: 'front#contact'
+    post '/send_email', to: 'front#send_email', as: :send_email
+  end
+  get '/:locale' => 'front#index'
+  match '*path', to: redirect("/#{I18n.default_locale}/#{path}")
+  match '', to: redirect("/#{I18n.default_locale}")
 end
